e0f4c3441a188f8767ad470d42cec521a6583f9d
Gulshan-codenet-softwares <gulshan.codenet@gmail.com>
2025-01-15 13:07:08 +0530

    solve structure issue

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -495,8 +495,32 @@ export const getAllGameData = async (req, res) => {
       `${baseURL}/api/get-active-market`,
     );
     const data = response.data.data
+    const foramtedData = [{
+        gameName    : data[0]?.gameName,
+        markets     : data.map((game)=>({
+        marketId    : game.marketId,
+        marketName  : game.marketName,
+        group_start : game.group_start,
+        group_end   : game.group_end,
+        series_start: game.series_start,
+        series_end  : game.series_end,
+        number_start: game.number_start,
+        number_end  : game.number_end,
+        start_time  : game.start_time,
+        end_time    : game.end_time,
+        date        : game.date,
+        price       : game.price,
+        isActive    : game.isActive,
+        isWin       : game.isWin,
+        isVoid      : game.isVoid,
+        createdAt   : game.createdAt,
+        updatedAt   : game.updatedAt
 
-    const combinedData = [...formattedGameData, ...data];
+        }))
+    }]
+
+  //console.log("formatedData........",foramtedData)
+    const combinedData = [...formattedGameData, ...foramtedData];
     res
       .status(statusCode.success)
       .json(
@@ -509,9 +533,9 @@ export const getAllGameData = async (req, res) => {
       );
   } catch (error) {
     if (error.response) {
-      return res.status(error.response.status).json(apiResponseErr(null, false, error.response.status, error.response.data.message || error.response.data.errMessage, res));
+      return res.status(error.response.status).json(apiResponseErr(null, false, error.response.status, error.response.data.message || error.response.data.errMessage));
     } else {
-      return res.status(statusCode.internalServerError).json(apiResponseErr(null, false, statusCode.internalServerError, error.message, res));
+      return res.status(statusCode.internalServerError).json(apiResponseErr(null, false, statusCode.internalServerError, error.message));
     };
   }
 };
