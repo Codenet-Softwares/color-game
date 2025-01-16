9c44795f5c85e21230fad6c6035932fd51f44f3d
riktadeyy <rikta.dey.cns@gmail.com>
2025-01-16 11:34:36 +0530

    update cron

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,6 @@ import BetHistory from './models/betHistory.model.js';
 import MarketBalance from './models/marketBalance.js';
 import { InactiveGameRoute } from './routes/inactiveGame.route.js';
 import InactiveGame from './models/inactiveGame.model.js';
-import { startMarketCountdown } from './cron/startMarketCountdown.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { Op } from "sequelize";
@@ -160,7 +159,7 @@ sequelize
 
     const updatedMarketsCache = new Map(); 
 
-    cron.schedule('*/2 * * * * *', async () => {
+    cron.schedule('* * * * * * *', async () => {
       try {
         const currentTime = getISTTime();
             
@@ -205,7 +204,6 @@ sequelize
           }
         }
    
-        if (updateMarket.length > 0) {
           clients.forEach((client) => {
             try {
               client.write(`data: ${JSON.stringify(updateMarket)}\n\n`);
@@ -215,7 +213,6 @@ sequelize
           });
     
           console.log(`[SSE] Updates broadcasted: ${JSON.stringify(updateMarket)}`);
-        }
     
       } catch (error) {
         console.error('Error checking market statuses:', error);
