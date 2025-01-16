8bd35b2d5a4418c92734e9521f8d2e5bf4759acb
riktadeyy <rikta.dey.cns@gmail.com>
2025-01-16 12:29:41 +0530

    update front end event url to server url

--- frontend-user/src/screen/common/updateMarketEvent.js
+++ frontend-user/src/screen/common/updateMarketEvent.js
@@ -1,4 +1,4 @@
-const eventSource = new EventSource("http://localhost:7000/events");
+const eventSource = new EventSource("https://cg.server.dummydoma.in/events");
 
 const updateMarketEventEmitter = () => {
     console.log("updatemarketEventemitter");
