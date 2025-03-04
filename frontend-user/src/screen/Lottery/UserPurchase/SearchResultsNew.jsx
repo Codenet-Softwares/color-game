bdc8e94c0da50f8ddc3f79ba8ad69cc62d3a606c
tamoghna-10 <137286113+tamoghna-10@users.noreply.github.com>
2025-03-04 17:34:20 +0530

    changes done in search lottery component

--- frontend-user/src/screen/Lottery/UserPurchase/SearchResultsNew.jsx
+++ frontend-user/src/screen/Lottery/UserPurchase/SearchResultsNew.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import TicketCard from "./TicketCard";
-import "./SearchResultsNew.css"; 
+import "./SearchResultsNew.css";
 
 const SearchResultsNew = ({ lotteryData, handleBack, handleBuy }) => {
-
   return (
     <div className="text-center ">
       {/* Back Button */}
       <div className="text-start ">
-        <button className="btn btn-secondary back-button text-uppercase" onClick={handleBack}>
+        <button
+          className="btn btn-secondary back-button text-uppercase"
+          onClick={handleBack}
+        >
           Back
         </button>
       </div>
@@ -32,13 +34,20 @@ const SearchResultsNew = ({ lotteryData, handleBack, handleBuy }) => {
               </div>
             </div>
             <h5 className="text-uppercase">
-              Price: <span className="result-price">₹{lotteryData.searchResult.price}</span>
+              Price:{" "}
+              <span className="result-price">
+                ₹{lotteryData.searchResult.price}
+              </span>
             </h5>
             <h5 className="text-uppercase">
-              SEM: <span className="result-sem">{lotteryData.searchResult.sem}</span>
+              SEM:{" "}
+              <span className="result-sem">{lotteryData.searchResult.sem}</span>
             </h5>
-             {/* Buy Button */}
-             <button className="btn btn-primary buy-button text-uppercase fw-bold " onClick={handleBuy}>
+            {/* Buy Button */}
+            <button
+              className="btn btn-dark buy-button text-uppercase fw-bold "
+              onClick={handleBuy}
+            >
               purchase
             </button>
           </>
