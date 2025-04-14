04d1df154bb0a137f70ea39e08a55f02d36ad08a
monika-codenet <codenetmonika@gmail.com>
2025-04-14 11:23:22 +0530

    fixed custom error handled

--- frontend-admin/src/Utils/helper.js
+++ frontend-admin/src/Utils/helper.js
@@ -1,16 +1,30 @@
+// export const customErrorHandler = (error) => {
+//     let errorMessage = ''
+//     if (error?.response?.data?.message) {
+//         errorMessage = error?.response?.data?.message
+//     } else if (error?.response?.data?.errMessage) {
+//         errorMessage = error?.response?.data?.errMessage
+//         if (error?.response?.data?.responseCode === 401) {
+//             window.location.href = '/'
+//             sessionStorage.removeItem("user");
+//             sessionStorage.removeItem("role");
+//         }
+//     } else {
+//         errorMessage = "something went wrong"
+//     }
+//     return errorMessage
+// }
+
+
 export const customErrorHandler = (error) => {
-    let errorMessage = ''
-    if (error?.response?.data?.message) {
-        errorMessage = error?.response?.data?.message
-    } else if (error?.response?.data?.errMessage) {
-        errorMessage = error?.response?.data?.errMessage
-        if (error?.response?.data?.responseCode === 401) {
-            window.location.href = '/'
-            sessionStorage.removeItem("user");
-            sessionStorage.removeItem("role");
-        }
-    } else {
-        errorMessage = "something went wrong"
+    const data = error?.response?.data;
+    const errorMessage = data?.message || data?.errMessage || "Something went wrong";
+
+    if (data?.responseCode === 401) {
+        sessionStorage.removeItem("user");
+        sessionStorage.removeItem("role");
+        window.location.href = '/';
     }
-    return errorMessage
+
+    return errorMessage;
 }
