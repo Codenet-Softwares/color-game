2841602f3a4e974a2fe62bf7531b38eb2e91e3a3
masudcns <masudur07cns@gmail.com>
2025-05-12 13:15:11 +0530

    *

--- backend/routes/wl.external.route.js
+++ backend/routes/wl.external.route.js
@@ -6,15 +6,15 @@ import customErrorHandler from "../middleware/customErrorHandler.js";
 
 
 export const wlExternalRoute = (app) => {
-    app.get('/api/slider-text-img',customErrorHandler,authorize([string.User]),  getSliderTextImg);
+    app.get('/api/slider-text-img',customErrorHandler,  getSliderTextImg);
 
-    app.get('/api/get-gif', customErrorHandler,authorize([string.User]), getGif);
+    app.get('/api/get-gif', customErrorHandler, getGif);
 
-    app.get('/api/get-game-img', customErrorHandler,authorize([string.User]), getGameImg);
+    app.get('/api/get-game-img', customErrorHandler, getGameImg);
 
     app.get('/api/get-inner-game-img', customErrorHandler,authorize([string.User]), getInnerImg);
 
-    app.get('/api/get-announcements', customErrorHandler,authorize([string.User]), getAnnouncement);
+    app.get('/api/get-announcements', customErrorHandler, getAnnouncement);
 
     app.get('/api/get-inner-announcements', customErrorHandler,authorize([string.User]), getInnerAnnouncement);
 }
