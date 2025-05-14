55c8aa1b6130208e0706c2ea5b597fd028104e11
himanshu10-kashyap <himanshudbg10@gmail.com>
2025-05-14 11:21:25 +0530

    Remove custom error handler from API routes for cleaner code

--- backend/routes/wl.external.route.js
+++ backend/routes/wl.external.route.js
@@ -6,15 +6,15 @@ import customErrorHandler from "../middleware/customErrorHandler.js";
 
 
 export const wlExternalRoute = (app) => {
-    app.get('/api/slider-text-img',customErrorHandler,  getSliderTextImg);
+    app.get('/api/slider-text-img', getSliderTextImg);
 
-    app.get('/api/get-gif', customErrorHandler, getGif);
+    app.get('/api/get-gif', getGif);
 
-    app.get('/api/get-game-img', customErrorHandler, getGameImg);
+    app.get('/api/get-game-img', getGameImg);
 
-    app.get('/api/get-inner-game-img', customErrorHandler,authorize([string.User]), getInnerImg);
+    app.get('/api/get-inner-game-img', getInnerImg);
 
-    app.get('/api/get-announcements', customErrorHandler, getAnnouncement);
+    app.get('/api/get-announcements', getAnnouncement);
 
-    app.get('/api/get-inner-announcements', customErrorHandler,authorize([string.User]), getInnerAnnouncement);
+    app.get('/api/get-inner-announcements', getInnerAnnouncement);
 }
