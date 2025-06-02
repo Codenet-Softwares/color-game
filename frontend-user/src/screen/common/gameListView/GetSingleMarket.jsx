81fe9a0aa5d56060da9d6f28295d617064ffa728
tamoghna-10 <137286113+tamoghna-10@users.noreply.github.com>
2025-06-02 12:05:28 +0530

    in progress

--- frontend-user/src/screen/common/gameListView/GetSingleMarket.jsx
+++ frontend-user/src/screen/common/gameListView/GetSingleMarket.jsx
@@ -64,113 +64,115 @@ const GetSingleMarket = () => {
         } mb-5`}
       >
         <AppDrawer showCarousel={true} isMobile={false} isHomePage={true}>
-          <div className="row p-0 m-0">
-            <div
-              className="col-12 p-2 mt-2 text-white fw-bold h6 "
-              style={{ backgroundColor: "#18ADC5", fontSize: "16px" }}
-            >
-              {user_gameWithMarketData[0]?.gameName}
-            </div>
-            {user_gameWithMarketData[0]?.markets.length > 0 ? (
-              <>
-                <div className="row px-0 m-0">
-                  <div className="col-8"></div>
-                  <div
-                    className="col-2 fw-bold rounded-top-3 text-uppercase"
-                    style={{
-                      background: "#80C2F1",
-                      fontSize: "14px",
-                      padding: "5px",
-                    }}
-                  >
-                    {" "}
-                    Back
-                  </div>
-                  <div
-                    className="col-2 fw-bold rounded-top-3 text-uppercase"
-                    style={{
-                      background: "#FAA9BA",
-                      fontSize: "14px",
-                      padding: "5px",
-                    }}
-                  >
-                    {" "}
-                    Lay
-                  </div>
-                </div>
-                {user_gameWithMarketData &&
-                  user_gameWithMarketData[0]?.markets.map((marketData) => {
-                    return (
-                      <div
-                        className="row py-1 px-0 m-0"
-                        style={{
-                          backgroundColor: "white",
-                          borderTop: "1px solid #ccc",
-                        }}
-                        key={marketData.marketId}
-                      >
-                        <Link
-                          className={`col-8 text-dark text-decoration-none text-nowrap`}
-                          style={{ fontSize: "14px" }}
-                          to={`/gameView/${user_gameWithMarketData[0]?.gameName?.replace(
-                            /\s/g,
-                            ""
-                          )}/${marketData?.marketName?.replace(/\s/g, "")}/${
-                            marketData?.marketId
-                          }`}
-                          onClick={() => handleMarketId(marketData?.marketId)}
-                        >
-                          <div className="d-flex flex-column flex-md-row">
-                            <span className="fw-bold me-2">
-                              {formatDate(marketData.startTime)}
-                            </span>
-                            <span
-                              className="fw-bold text-primary text-wrap"
-                              style={{
-                                wordBreak: "break-word",
-                                whiteSpace: "normal",
-                              }}
-                            >
-                              {marketData.marketName}
-                            </span>
-                          </div>
-                        </Link>
-                        <div
-                          className="col-2 rounded fw-bold text-center text-md-start"
-                          style={{
-                            backgroundColor: "#80C2F1",
-                            fontSize: "13px",
-                            padding: "5px",
-                          }}
-                        >
-                          {marketData?.runners[0]?.rate[0]?.back ?? "N/A"}
-                        </div>
-                        <div
-                          className="col-2 rounded fw-bold text-center text-md-start"
-                          style={{
-                            backgroundColor: "#FAA9BA",
-                            fontSize: "13px",
-                            padding: "5px",
-                          }}
-                        >
-                          {marketData?.runners[0]?.rate[0]?.lay ?? "N/A"}
-                        </div>
-                      </div>
-                    );
-                  })}
-              </>
-            ) : (
-              <p
-                className="text-center fw-bold"
-                style={{
-                  backgroundColor: "#FAA9BA",
-                  fontSize: "13px",
-                  padding: "5px",
-                }}
+          <div>
+            <div className="row p-0 m-0">
+              <div
+                className="col-12 p-2 mt-2 text-white fw-bold h6 "
+                style={{ backgroundColor: "#18ADC5", fontSize: "16px" }}
               >
-                No market Available
-              </p>
-            )}
+                {user_gameWithMarketData[0]?.gameName}
+              </div>
+              {user_gameWithMarketData[0]?.markets.length > 0 ? (
+                <>
+                  <div className="row px-0 m-0">
+                    <div className="col-8"></div>
+                    <div
+                      className="col-2 fw-bold rounded-top-3 text-uppercase"
+                      style={{
+                        background: "#80C2F1",
+                        fontSize: "14px",
+                        padding: "5px",
+                      }}
+                    >
+                      {" "}
+                      Back
+                    </div>
+                    <div
+                      className="col-2 fw-bold rounded-top-3 text-uppercase"
+                      style={{
+                        background: "#FAA9BA",
+                        fontSize: "14px",
+                        padding: "5px",
+                      }}
+                    >
+                      {" "}
+                      Lay
+                    </div>
+                  </div>
+                  {user_gameWithMarketData &&
+                    user_gameWithMarketData[0]?.markets.map((marketData) => {
+                      return (
+                        <div
+                          className="row py-1 px-0 m-0"
+                          style={{
+                            backgroundColor: "white",
+                            borderTop: "1px solid #ccc",
+                          }}
+                          key={marketData.marketId}
+                        >
+                          <Link
+                            className={`col-8 text-dark text-decoration-none text-nowrap`}
+                            style={{ fontSize: "14px" }}
+                            to={`/gameView/${user_gameWithMarketData[0]?.gameName?.replace(
+                              /\s/g,
+                              ""
+                            )}/${marketData?.marketName?.replace(/\s/g, "")}/${
+                              marketData?.marketId
+                            }`}
+                            onClick={() => handleMarketId(marketData?.marketId)}
+                          >
+                            <div className="d-flex flex-column flex-md-row">
+                              <span className="fw-bold me-2">
+                                {formatDate(marketData.startTime)}
+                              </span>
+                              <span
+                                className="fw-bold text-primary text-wrap"
+                                style={{
+                                  wordBreak: "break-word",
+                                  whiteSpace: "normal",
+                                }}
+                              >
+                                {marketData.marketName}
+                              </span>
+                            </div>
+                          </Link>
+                          <div
+                            className="col-2 rounded fw-bold text-center text-md-start"
+                            style={{
+                              backgroundColor: "#80C2F1",
+                              fontSize: "13px",
+                              padding: "5px",
+                            }}
+                          >
+                            {marketData?.runners[0]?.rate[0]?.back ?? "N/A"}
+                          </div>
+                          <div
+                            className="col-2 rounded fw-bold text-center text-md-start"
+                            style={{
+                              backgroundColor: "#FAA9BA",
+                              fontSize: "13px",
+                              padding: "5px",
+                            }}
+                          >
+                            {marketData?.runners[0]?.rate[0]?.lay ?? "N/A"}
+                          </div>
+                        </div>
+                      );
+                    })}
+                </>
+              ) : (
+                <p
+                  className="text-center fw-bold"
+                  style={{
+                    backgroundColor: "#FAA9BA",
+                    fontSize: "13px",
+                    padding: "5px",
+                  }}
+                >
+                  No market Available
+                </p>
+              )}
+            </div>
           </div>
         </AppDrawer>
       </div>
